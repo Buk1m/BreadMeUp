b0a09241f5cb6a1bcd769763df199e56cdbb6b33
Paweł Pomarański <pawelpomaranski97@gmail.com>
2019-05-19 17:40:06 +0200

    Add version annotation to user and role entities

--- src/main/java/com/pkkl/BreadMeUp/model/User.java
+++ src/main/java/com/pkkl/BreadMeUp/model/User.java
@@ -39,4 +39,8 @@ public class User {
     @ManyToMany(cascade = CascadeType.ALL)
     @JoinTable(name = "users_roles", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
     private Set<Role> roles;
+
+    @Version
+    @Column(name = "version")
+    private long version;
 }
