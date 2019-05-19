869ec6755839e3a3db4d8baeb66f49738505e32d
Paweł Pomarański <41118995+pomaranski@users.noreply.github.com>
2019-05-20 00:24:33 +0200

    Feature/registration (#9)
    * Added blank constraints on fields where necessary, constructors, builder
    
    * Added possibility to find role by name
    
    * Added init data script for database
    
    * Added h2 dependency to pom for unit tests
    
    * Added user service with registration feature and tested it
    
    * Change test names

--- src/main/java/com/pkkl/BreadMeUp/repositories/RoleRepository.java
+++ src/main/java/com/pkkl/BreadMeUp/repositories/RoleRepository.java
@@ -4,6 +4,10 @@ import com.pkkl.BreadMeUp.model.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface RoleRepository extends JpaRepository<Role, Integer> {
+
+    Optional<Role> findByName(String name);
 }
