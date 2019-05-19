f7f9ebfb37e7e6d19433d7c92cf62b61c3954e20
Paweł Pomarański <pawelpomaranski97@gmail.com>
2019-05-19 22:01:44 +0200

    Added blank constraints on fields where necessary, constructors, builder

--- src/main/java/com/pkkl/BreadMeUp/model/Role.java
+++ src/main/java/com/pkkl/BreadMeUp/model/Role.java
@@ -1,11 +1,18 @@
 package com.pkkl.BreadMeUp.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.hibernate.validator.constraints.Length;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 
 @Entity
 @Table(name = "roles")
@@ -18,6 +25,7 @@ public class Role {
 
     @Column(name = "name", unique = true)
     @Length(min = 3, max = 20, message = "Name length should be from 3 to 20")
+    @NotBlank(message = "Name cannot be blank")
     private String name;
 
     @Version
--- src/main/java/com/pkkl/BreadMeUp/model/User.java
+++ src/main/java/com/pkkl/BreadMeUp/model/User.java
@@ -1,14 +1,21 @@
 package com.pkkl.BreadMeUp.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.hibernate.validator.constraints.Length;
 
 import javax.persistence.*;
 import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
 import java.util.Set;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 
 @Entity
 @Table(name = "users")
@@ -21,19 +28,23 @@ public class User {
 
     @Column(name = "login", unique = true)
     @Length(min = 3, max = 64, message = "Login length should be from 3 to 64")
+    @NotBlank(message = "Login cannot be blank")
     private String login;
 
     @Column(name = "password", unique = true)
     @Length(min = 8, max = 64, message = "Password length should be from 8 to 64")
+    @NotBlank(message = "Password cannot be blank")
     private String password;
 
     @Column(name = "email", unique = true)
     @Email(message = "Email is not valid")
     @Length(max = 64, message = "Email length should be to 64")
+    @NotBlank(message = "Email cannot be blank")
     private String email;
 
     @Column(name = "phone", unique = true)
     @Pattern(regexp = "\\d{7,15}", message = "Phone is not valid")
+    @NotBlank(message = "Phone cannot be blank")
     private String phone;
 
     @ManyToMany(cascade = CascadeType.ALL)
