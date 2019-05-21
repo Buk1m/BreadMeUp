4ee9215ccfc79a21c49f62889a0c15c912817c1c
Paweł Pomarański <pawelpomaranski97@gmail.com>
2019-05-21 22:53:04 +0200

    Added user service unit tests, refactored other

--- src/main/java/com/pkkl/BreadMeUp/services/UserServiceImpl.java
+++ src/main/java/com/pkkl/BreadMeUp/services/UserServiceImpl.java
@@ -10,7 +10,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.InvalidDataAccessApiUsageException;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.validation.ConstraintViolationException;
