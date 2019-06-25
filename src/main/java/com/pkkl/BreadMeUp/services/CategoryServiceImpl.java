07fcd0524b25f3319c7991eba6f45966ec6a35d9
PostAdam <35382123+PostAdam@users.noreply.github.com>
2019-06-25 23:03:51 +0200

    added logging in bakery, base and category services (#52)

--- src/main/java/com/pkkl/BreadMeUp/services/CategoryServiceImpl.java
+++ src/main/java/com/pkkl/BreadMeUp/services/CategoryServiceImpl.java
@@ -3,10 +3,13 @@ package com.pkkl.BreadMeUp.services;
 import com.pkkl.BreadMeUp.exceptions.DatabaseException;
 import com.pkkl.BreadMeUp.model.Category;
 import com.pkkl.BreadMeUp.repositories.CategoryRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+
+@Slf4j
 
 @Service
 public class CategoryServiceImpl implements CategoryService {
@@ -23,6 +26,7 @@ public class CategoryServiceImpl implements CategoryService {
         try {
             return this.categoryRepository.findAll();
         } catch (Exception e) {
+            log.error(e.getMessage(), e);
             throw new DatabaseException(e);
         }
     }
@@ -32,6 +36,7 @@ public class CategoryServiceImpl implements CategoryService {
         try {
             return this.categoryRepository.findById(id).orElseThrow(() -> new RuntimeException("Category type doesn't exist"));
         } catch (Exception e) {
+            log.error(e.getMessage(), e);
             throw new DatabaseException(e);
         }
     }
